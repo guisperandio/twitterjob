311e83fb1d8f3063d0146efd2ef7e88213bb8ae4
guisperandio <guisperandio89@gmail.com>
2018-03-02 09:47:24 +0000

    Initial commit

---
+++ server/routes/api.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+let twit = require('twitter');
+
+let secret = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+
+let twitter = new twit(secret);
+
+router.get('/tweets', function(req, res, next) {
+  twitter.get('statuses/user_timeline',
+    {screen_name: 'tproworld', count : 30},
+    function(error, tweets, response){
+      if(!error){
+        res.status(200).json({tweets});
+      } else{
+        res.status(500).json({ error: error });
+      }
+    }
+  );
+});
+
+module.exports = router;
